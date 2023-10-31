b814b59b51f22a75ded274d8c43edba04940f163
saghro <ayoubsaghro27@gmail.com>
2023-10-31 15:52:02 +0100

    c

--- LIKE_FACEBOOK/LikeCompenent.js
+++ LIKE_FACEBOOK/LikeCompenent.js
@@ -1,20 +1,23 @@
-var LikeCompenent = /** @class */ (function () {
-    function LikeCompenent(initialLikes) {
+var LikeComponent = /** @class */ (function () {
+    function LikeComponent(initialLikes) {
         if (initialLikes === void 0) { initialLikes = 0; }
         this.likes = initialLikes;
         this.isLiked = false;
     }
-    LikeCompenent.prototype.onclick = function () {
+    LikeComponent.prototype.onclick = function () {
         this.isLiked = !this.isLiked;
         this.likes += this.isLiked ? 1 : -1;
         this.logStatus();
     };
-    LikeCompenent.prototype.logStatus = function () {
+    LikeComponent.prototype.logStatus = function () {
+        console;
         console.log("J'aime: ".concat(this.likes, " ").concat(this.isLiked ? "sélectionné" : "non sélectionné"));
     };
-    return LikeCompenent;
+    return LikeComponent;
 }());
-var likeCompenent = new LikeCompenent(5);
-likeCompenent.onclick();
-likeCompenent.onclick();
-likeCompenent.onclick();
+var likeComponent = new LikeComponent(5);
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();
--- LIKE_FACEBOOK/LikeCompenent.ts
+++ LIKE_FACEBOOK/LikeCompenent.ts
@@ -1,21 +1,41 @@
-class LikeCompenent {
-    private likes: number;
-    private isLiked: boolean;
-  
-    constructor(initialLikes: number = 0) {
+export class LikeComponent{
+   constructor(private _likesNumber : number, private _isliked:boolean){
+
+   }
+   get likesNumber(){
+    return this._likesNumber
+   }
+   get isLiked
+}
+
+
+
+
+
+/*class LikeComponent {
+  private likes: number;
+  private isLiked: boolean;
+
+  constructor(initialLikes: number = 0) {
       this.likes = initialLikes;
       this.isLiked = false;
-    }
-    onclick(){
+  }
+
+  onclick() {
       this.isLiked = !this.isLiked;
       this.likes += this.isLiked ? 1 : -1;
       this.logStatus();
-    }
-    private logStatus() {
+  }
+
+  private logStatus() {
+    console
       console.log(`J'aime: ${this.likes} ${this.isLiked ? "sélectionné" : "non sélectionné"}`);
-    }
+  }
 }
-const likeCompenent = new LikeCompenent(5);
-likeCompenent.onclick();
-likeCompenent.onclick();
-likeCompenent.onclick();
+
+const likeComponent = new LikeComponent(5);
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();
+likeComponent.onclick();**/
